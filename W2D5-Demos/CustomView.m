177df1790ce338786f96484b84740acbbcf93efc
steve thompson <steven.thompson.ca@gmail.com>
2017-03-16 17:06:37 -0400

    final commit

---
+++ W2D5-Demos/CustomView.m
@@ -0,0 +1,16 @@
+//
+//  CustomView.m
+//  W2D5-Demos
+//
+//  Created by steve on 2017-03-16.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+  NSLog(@"something");
+}
+@end
