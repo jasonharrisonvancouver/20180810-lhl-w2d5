1ca4d17b8b33f6393ec4165b9df3de3bfc7c5992
steve thompson <steven.thompson.ca@gmail.com>
2017-03-16 14:35:50 -0400

    Refactors textview.

--- W2D5-Demos/TextViewViewController.m
+++ W2D5-Demos/TextViewViewController.m
@@ -10,30 +10,49 @@
 
 @interface TextViewViewController ()<UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextView *textView;
-
 @end
 
 @implementation TextViewViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  [self setupTextView];
+  [self setupStyle];
+}
+
+- (void)setupTextView {
   self.textView.text = @"";
   self.automaticallyAdjustsScrollViewInsets = NO;
 }
 
+- (void)setupStyle {
+  self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+  self.navigationController.navigationBar.translucent = YES;
+}
 
 - (IBAction)save:(UIBarButtonItem *)sender {
-  NSLog(@"%s", __PRETTY_FUNCTION__);
-  NSLog(@"%@", self.textView.text);
+  NSLog(@"%d: %s", __LINE__, __PRETTY_FUNCTION__);
+  NSLog(@"%d: %@", __LINE__, self.textView.text);
   [self.textView resignFirstResponder];
 }
 
-
+#pragma mark - Delegate Methodsw
 
 - (void)textViewDidBeginEditing:(UITextView *)textView {
   // clears the text
   NSLog(@"%s", __PRETTY_FUNCTION__);
   self.textView.text = @"";
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+  NSLog(@"%d: %s", __LINE__, __PRETTY_FUNCTION__);
+  NSLog(@"%d: %@", __LINE__, text);
+  return YES;
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+  NSLog(@"%d: %s", __LINE__, __PRETTY_FUNCTION__);
+  NSLog(@"%d: %@", __LINE__, textView.text);
 }
 
 @end
